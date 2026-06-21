83e744d5d2ef1220e68d0ef89026666d0dc551ce
Hadhi Noor Mohamed H <hadhinoor129@gmail.com>
2026-06-22 02:01:21 +0530

    Java program to enter and display a number

---
+++ enteranumber.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.*;
+public class file {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scan.nextInt();
+        System.out.println("You entered: " + n);
+    }
+}
